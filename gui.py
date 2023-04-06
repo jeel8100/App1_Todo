8e4357b2484d4d26185801e8a5fb9c2a139df62d
jpatel8100 <111167939+jpatel8100@users.noreply.github.com>
2023-04-05 23:19:45 -0300

    GUI

---
+++ gui.py
@@ -0,0 +1,12 @@
+import Modules
+import PySimpleGUI as sg
+
+label = sg.Text("Type in Todo list")
+input_box = sg.InputText(tooltip="Enter todo")
+add_button = sg.Button("Add")
+
+## window accepts the objects row by row
+window = sg.Window('My TO-Do App', layout =[[label], [input_box],[add_button]])
+
+window.read()
+window.close() 
