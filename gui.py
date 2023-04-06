7aa379dfc698b1fc4020b33444592bb6a2e81db2
jpatel8100 <111167939+jpatel8100@users.noreply.github.com>
2023-04-05 23:32:28 -0300

    xyz

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
 ## window accepts the objects row by row
+##fjjfj
 window = sg.Window('My TO-Do App', layout =[[label], [input_box],[add_button]])
 
 window.read()
